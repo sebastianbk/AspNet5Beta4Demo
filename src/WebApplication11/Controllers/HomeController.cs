a4f980f1d1ff470af63b521e20e03946190c8d5f
Sebastian Brandes Kraaijenzank <sb@sebastianbrandes.com>
2015-05-06 13:20:00 +0200

    Initial solution

---
+++ src/WebApplication11/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using WebApplication11.Models;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApplication11.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            var post = new BlogPost
+            {
+                PostId = 123,
+                Title = "Hello",
+                Description = "This is a test!"
+            };
+
+            return View(post);
+        }
+    }
+}
